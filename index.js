563550c5ce17f7b3df0fd87a4586577b5b4d3010
sandiarbaa <sandiarbaa@gmail.com>
2024-08-25 13:02:00 +0700

    first setup

---
+++ index.js
@@ -0,0 +1,29 @@
+// setup lib
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// template engine ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// connect to mongodb
+mongoose
+  .connect("mongodb://127.0.0.1/shop_db")
+  .then((result) => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// route web path
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// port
+app.listen(3000, () => {
+  console.log("shop app listening on http://127.0.0.1:3000");
+});

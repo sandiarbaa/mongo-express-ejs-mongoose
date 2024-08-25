30eb6b408bfde5bb11af3c4c445b80e8202608f4
sandiarbaa <sandiarbaa@gmail.com>
2024-08-25 16:56:55 +0700

    restful save product

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 // template engine ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true })); // agar express dapat mengambil data yg dikirimkan dari body request
 
 /* Models */
 const Product = require("./models/product");
@@ -36,6 +37,23 @@ app.get("/products", async (req, res) => {
 // form tambah data product
 app.get("/products/create", (req, res) => {
   res.render("products/create");
+});
+
+// tambah data product
+app.post("/products", async (req, res) => {
+  // variabel untuk mendapatkan data product nya
+  // buat data menggunakan model product
+  // dan req.body itu akan membawa data yg di dapat dari form
+  const product = new Product(req.body); // data product sudah dibuat dengan format model product
+  // kalau format modelnya sudah ada berserta datanya, bisa langsung panggil method save
+  // dari variabel yg menyimpan object yg berisi data productnya
+  await product.save();
+  // sesudah data di simpan, baru redirect ke halaman products atau detail product
+  res.redirect(`/products/${product._id}`);
+  // kita bisa mengakses id dari product yg baru saja di simpan
+  // karena sebetulnya data dari object nya itu sudah didapatkan sebelum datanya di simpan
+  // id atau properti lainnya didapatkan pada saat membuat object product yg sudah ada datanya
+  // jadi bisa dimanfaatkan sesuai kebutuhan di route ini
 });
 
 // detail product

071f23a06aada237b5cc1177a0f432ee3666a6d8
sandiarbaa <sandiarbaa@gmail.com>
2024-08-29 16:27:25 +0700

    error handling di async function #4

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const app = express();
 
+const ErrorHandler = require("./ErrorHandler");
+
 // template engine ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -45,6 +47,7 @@ app.get("/products", async (req, res) => {
 
 // form tambah data product
 app.get("/products/create", (req, res) => {
+  // throw new ErrorHandler("This is custom error", 503);
   res.render("products/create");
 });
 
@@ -57,36 +60,55 @@ app.post("/products", async (req, res) => {
 });
 
 // detail product
-app.get("/products/:id", async (req, res) => {
-  // query
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render("products/show", { product });
+// error handling di async function
+app.get("/products/:id", async (req, res, next) => {
+  try {
+    // query
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/show", { product });
+  } catch (err) {
+    // gunakan next supaya dia menjalankan middleware error-handling yg di bawah itu
+    // karena itu kan middleware, dan next akan mengecek ada middleware lagi ngga, kalo ada ya lanjut ke situ
+    next(new ErrorHandler("Product not found", 404));
+  }
 });
 
 // form edit data product
-app.get("/products/:id/edit", async (req, res) => {
-  // query
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render("products/edit", { product });
+app.get("/products/:id/edit", async (req, res, next) => {
+  try {
+    // query
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/edit", { product });
+  } catch (err) {
+    next(new ErrorHandler("Product not found", 404));
+  }
 });
 
 // edit data product
-app.put("/products/:id", async (req, res) => {
-  // query
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-  });
-  res.redirect(`/products/${product._id}`); // redirect ke halaman show product
+app.put("/products/:id", async (req, res, next) => {
+  try {
+    // query
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+    });
+    res.redirect(`/products/${product._id}`); // redirect ke halaman show product
+  } catch (err) {
+    next(new ErrorHandler("Failed update data", 412));
+  }
 });
 
 // delete products
-app.delete("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  await Product.findByIdAndDelete(id);
-  res.redirect("/products");
+app.delete("/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect("/products");
+  } catch (err) {
+    next(new ErrorHandler("Failed delete data", 412));
+  }
 });
 
 app.get("/categories", async (req, res) => {
@@ -94,7 +116,19 @@ app.get("/categories", async (req, res) => {
   res.render("products/categories", { categories });
 });
 
+// middleware error-handling
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).send(message);
+});
+
 // port
 app.listen(3000, () => {
   console.log("shop app listening on http://127.0.0.1:3000");
 });
+
+// error handling
+// buat objek errornya dulu
+// lalu lakukan proses error handling di route, gunakan next
+// kalau errornya tidak ada akan pakai default user-defined middleware error-handling
+// kalau ada ya pakai error yg kita definisikan
